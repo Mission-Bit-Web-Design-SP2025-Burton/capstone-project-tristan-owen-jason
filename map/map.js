629fce1efc2c71e489a81dc678646b4ff0cec1e3
Owen <owcheng3@gmail.com>
2025-04-24 19:24:43 +0000

    Apr 24, 2025, 12:24 PM

--- map/map.js
+++ map/map.js
@@ -1,6 +1,35 @@
-var r = document.querySelector(":root");
-
+let r = document.querySelector(":root");
+let increment = 100;
 function IncreaseImageSize() {
-  var rs = getComputedStyle(r);
-  r.style.setProperty("--size", "200px");
+  let rs = getComputedStyle(r);
+  let rsSize = rs.getPropertyValue("--size");
+  let value = parseInt(rsSize.replace(/px/, "")) + increment + "px";
+  r.style.setProperty("--size", value);
+  console.log(value);
 }
+function DecreaseImageSize() {
+  let rs = getComputedStyle(r);
+  let rsSize = rs.getPropertyValue("--size");
+  let value = parseInt(rsSize.replace(/px/, "")) - increment + "px";
+  if (parseInt(value.replace(/px/, "")) <= 0) {
+    value = "100px";
+  }
+  r.style.setProperty("--size", value);
+  console.log(value);
+}
+
+const img = document.getElementById("test-img");
+
+img.addEventListener("dragstart", (e) => {
+  e.preventDefault();
+  e.dataTransfer.setData("text/plain", e.target.id || "dragged-image");
+  e.dataTransfer.effectAllowed = "move";
+});
+
+img.addEventListener("drag", (e) => {
+  console.log("dragging image");
+});
+
+img.addEventListener("dragend", (e) => {
+  console.log("drag ended");
+});
